ec471fa5cd16dc76e37772f6cc21a38082357d58
unknown <mahmudhasan.hb@gmail.com>
2025-05-16 19:41:05 +0600

    completed  model

--- src/redux/api/baseApi.tsx
+++ src/redux/api/baseApi.tsx
@@ -4,8 +4,8 @@ import Cookies from "js-cookie";
 export const baseApi = createApi({
   reducerPath: "baseApi", // The key for this API in the Redux store
   baseQuery: fetchBaseQuery({
-    // baseUrl: 'https://api.boffo-global.com/api/v1', // Replace with your API's base URL
-    baseUrl: "http://localhost:4000/api/v1",
+    baseUrl: 'https://api.boffo-global.com/api/v1', // Replace with your API's base URL
+    // baseUrl: "http://localhost:4000/api/v1",
     // credentials : "include",
     prepareHeaders: (headers) => {
       const token = Cookies?.get("token"); // Assuming token is stored in the auth slice
